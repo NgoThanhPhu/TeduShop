473a79d01c48e27bdc190122631465f68e243026
NGOTHANHPHU <ngothanhphu01@gmail.com>
2021-01-09 13:59:09 +0700

    Push Model Directory

---
+++ TeduShop.Model/Models/MenuGroup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeduShop.Model.Models
+{
+    [Table("MenuGroups")]
+    public class MenuGroup
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { set; get; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { set; get; }
+
+        public virtual IEnumerable<Menu> Menus { set; get; }
+        /* When Menu query to MenuGroups.Menu to collect all Menu
+           MenuGroup contact with Menu
+         */
+    }
+}
